c1e64991da5669b94edfb6d04f263bbb4b036e1a
SamaDev200 <darbeksaman320@gmail.com>
2026-07-14 19:06:51 +0500

    Fix settings for Render

--- backend/server_logic/settings.py
+++ backend/server_logic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x8(rx%y0+dnl9&crb&n4pols6&9h#wfa!=mj3)$7w7&i3b+4q%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = ('localhost:3000/')
