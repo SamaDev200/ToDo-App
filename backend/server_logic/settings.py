a888b9cb21bdb01e45c6db8b79a9b7368c805759
SamaDev200 <darbeksaman320@gmail.com>
2026-07-14 19:16:25 +0500

    Add PostgreSQL support via dj_database_url

--- backend/server_logic/settings.py
+++ backend/server_logic/settings.py
@@ -88,12 +88,18 @@ WSGI_APPLICATION = 'server_logic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+if db_from_env:
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
